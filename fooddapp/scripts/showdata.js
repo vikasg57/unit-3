30ef366701934c4b8f1071c3a9dff161b1afe618
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-01-09 10:34:33 +0530

    search page updated

--- fooddapp/scripts/showdata.js
+++ fooddapp/scripts/showdata.js
@@ -13,12 +13,15 @@ async function getdata(url){
 }
 
 function searchresult(data,location){
+     document.querySelector('#results').style.opacity="1"
     location.innerHTML=null
+
     
 
     //location as a argument so we can append data on any location 
 
     data.map((el)=>{
+        let{strArea,strMealThumb,strMeal}=el
 
 
 
@@ -29,18 +32,65 @@ function searchresult(data,location){
         let infodiv=document.createElement('div')
         infodiv.setAttribute('class',"infodiv")
         let area=document.createElement('h5')
-        area.innerHTML=el.strArea
+        area.innerHTML=strArea
         let img=document.createElement('img')
-        img.src=el.strMealThumb
+        img.src=strMealThumb
         let title=document.createElement('h3')
-        title.innerHTML=el.strMeal
-        infodiv.append(title,area)
+        title.innerHTML=strMeal
+        infodiv.append(title)
+        div.append(img,infodiv)
+
+        location.append(div)
+
+        div.addEventListener('click',function(){
+            getitem(el,maindiv)
+        })
+       
+
+
+    })
+
+     
+}
+
+let getitem=(el,location)=>{
+    console.log(el)
+   
+
+     let{strArea,strMealThumb,strMeal}=el
+
+     console.log(strArea,strMeal,strMealThumb)
+
+     console.log(el.strArea)
+        let div=document.createElement('div')
+        div.setAttribute('class',"mainbox")
+         let infodiv=document.createElement('div')
+        infodiv.setAttribute('class',"maininfodiv")
+         let area=document.createElement('h3')
+        area.innerHTML=strArea
+        let img=document.createElement('img')
+        img.src=strMealThumb
+        let title=document.createElement('h3')
+        title.innerHTML=strMeal
+
+
+        infodiv.append(title,area,)
+
         div.append(img,infodiv)
 
         location.append(div)
 
 
-    })
+
+    
+
+
+
+
 }
 
-export {getdata,searchresult}
+
+
+
+
+export {getdata,searchresult,getitem}
